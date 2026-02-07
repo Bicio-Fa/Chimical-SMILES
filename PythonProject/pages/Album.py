0940d8faf631480ae43bbd1d53ee7add2b35aa99
Bicio-Fa <lfabricio0202fbfl@gmail.com>
2026-02-06 22:48:23 -0300

    Album.py

--- PythonProject/pages/Album.py
+++ PythonProject/pages/Album.py
@@ -2,9 +2,10 @@ import os
 import streamlit as st
 
 get_path=os.getcwd()
-list_txt=[t for t in os.listdir(get_path+r'\Album') if t.endswith('.png')]
+list_txt=[t for t in os.listdir(get_path+r'/Album') if t.endswith('.png')]
 
 for item in list_txt:
-    path = get_path + r'\Album' + rf'\{item}'
+    path = get_path + r'/Album' + rf'/{item}'
     st.image(path,caption=item.replace('.png',''))
+
 
