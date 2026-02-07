06011daadccf93d26503cb4511c6f427f3c056e4
Bicio-Fa <lfabricio0202fbfl@gmail.com>
2026-02-06 23:11:44 -0300

    Update Album.py
    Add dele button

--- PythonProject/pages/Album.py
+++ PythonProject/pages/Album.py
@@ -2,10 +2,17 @@ import os
 import streamlit as st
 
 get_path=os.getcwd()
-list_txt=[t for t in os.listdir(get_path+r'/Album') if t.endswith('.png')]
+list_png=[t for t in os.listdir(get_path+r'/Album') if t.endswith('.png')]
+def delet_img(name_):
+    if os.path.exists(get_path+r'/Album' + fr'/{name_}'):
+        os.remove(get_path+r'/Album' + fr'/{name_}')
+        st.rerun()
 
-for item in list_txt:
+for item in list_png:
     path = get_path + r'/Album' + rf'/{item}'
     st.image(path,caption=item.replace('.png',''))
-
+    with st.popover('Delet'):
+        st.warning('Are you sure you want to delete this molecule?')
+        if st.button('Confirm'):
+            delet_img(item)
 
